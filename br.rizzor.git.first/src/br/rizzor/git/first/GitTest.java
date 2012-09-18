6a12e42afe102353494294f1c5c9bc43eddc964b
Reynaldo J. Abreu Rizzo <reyabreu@gmail.com>
2012-09-18 08:41:13 -0300

    Initial Commit

---
+++ br.rizzor.git.first/src/br/rizzor/git/first/GitTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.rizzor.git.first;
+
+/**
+ * This is intended to showcase Git's power
+ * 
+ * @author Reynaldo
+ * 
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Make it Git!");
+	}
+
+}
